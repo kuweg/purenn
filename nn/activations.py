add stable softmax, small fixes

nn/activations.py
@@ -3,7 +3,7 @@ from typing import Callable, Union
 import numpy as np
 
 from .exceptions import UnknownActivationError
-from .utils import set_repr
+from .utils import dummy_callable
 
 
 class Activation(ABC):
@@ -64,15 +64,23 @@ class Sigmoid(Activation):
 
 class Softmax(Activation):
     
-    def activation(self, array: np.ndarray, stable: bool=False) -> np.ndarray:
-        if stable:
-            array = array - np.max(array)
+    def activation(self, array: np.ndarray) -> np.ndarray:
         return np.exp(array) / np.sum(np.exp(array), axis=0)
     
     def df(self, array: np.ndarray) -> np.ndarray:
         I = np.eye(array.shape[0])
-        return self.activation(array) * (I - self.activation(array).T) 
+        return self.activation(array) * (I - self.activation(array).T)
+    
 
+class StableSoftmax(Softmax):
+    
+    def activation(self, array: np.ndarray) -> np.ndarray:
+        array = array - np.max(array)
+        return np.exp(array) / np.sum(np.exp(array), axis=0)
+    
+    def __repr__(self) -> str:
+        return 's_softmax'
+    
 # @set_repr('softmax')
 # def softmax(array: np.ndarray) -> np.ndarray:
 #     shifted_array = array - np.max(array)
@@ -86,13 +94,16 @@ tanh = Tanh()
 sigmoid = Sigmoid()
 leaky_relu = LeakyRelu()
 softmax = Softmax()
+s_softmax = StableSoftmax()
 
 ACTIVATION_MAP ={
     'relu': relu,
     'tanh': tanh,
     'sigmoid': sigmoid,
     'softmax': softmax,
-    'leaky_relu': leaky_relu
+    'leaky_relu': leaky_relu,
+    's_softmax': s_softmax,
+    'none': dummy_callable
 }
 
 
@@ -113,4 +124,4 @@ def set_activation(activation: Union[Callable, str]) -> Callable:
     
 
 def is_softmax(activation_fn: Callable) -> bool:
-    return activation_fn is softmax
+    return activation_fn is softmax or activation_fn is s_softmax

nn/layers.py
@@ -57,7 +57,7 @@ class WeightsLayer(Layer):
         self.z = np.dot(self.weights, input_data) + self.bias
         self.a = self.activation(self.z)
         return self.a
-    
+
     def backward(self, error: np.ndarray) -> np.ndarray:
         batch_size = self.input.shape[1]
         if is_softmax(self.activation):
@@ -77,7 +77,7 @@ class WeightsLayer(Layer):
         
     
     def __repr__(self) -> str:
-        return "({}, {})|{}|{}|{}".format(
+        return "({}, {}) | {} | {} | {}".format(
             self.n_input_nodes,
             self.n_output_nodes,
             self.bias.shape,

nn/loss.py
@@ -41,27 +41,27 @@ class MeanAbsoluteError(Loss):
         return -((y_true - y_pred) / (abs(y_true - y_pred) + 10**-100))/n
 
 
-class CategoricalCrossEntropy(Loss):
-    """
-    This function calculates and return the categorical-crossentropy-loss.
-    "+1e-15" is just for adding a very small number to avoid np.log(0).
+# class CategoricalCrossEntropy(Loss):
+#     """
+#     This function calculates and return the categorical-crossentropy-loss.
+#     "+1e-15" is just for adding a very small number to avoid np.log(0).
     
-    :param y_true: the current predicted output of the model
-    :type y_true: np.ndarray
-    :param y_pred: the expected output
-    :type y_rped: np.ndarray
-    :return: the categorical-crossentropy-loss
-    :rtype: np.float64
-    """
+#     :param y_true: the current predicted output of the model
+#     :type y_true: np.ndarray
+#     :param y_pred: the expected output
+#     :type y_rped: np.ndarray
+#     :return: the categorical-crossentropy-loss
+#     :rtype: np.float64
+#     """
     
-    @staticmethod
-    def calc(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
-        loss = -np.sum(y_true * (np.log(y_pred+1e-10)))
-        return loss / float(y_pred.shape[0])
+#     @staticmethod
+#     def calc(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
+#         loss = -np.sum(y_true * (np.log(y_pred+1e-10)))
+#         return loss / float(y_pred.shape[0])
     
-    @staticmethod
-    def df(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
-        return y_pred - y_true
+#     @staticmethod
+#     def df(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
+#         return y_pred - y_true
 
 
 class CrossEntropyLoss(Loss):

nn/utils.py
@@ -47,7 +47,8 @@ def apply_function_to_nparray(array: np.ndarray, fn: Callable) -> np.ndarray:
 def flatten_list(lst: List[List]) -> List[Any]:
     return [item for sublist in lst for item in sublist]
 
- 
+
+@set_repr('None')
 def dummy_callable(*args: Any) -> Any:
     return args
 
@@ -69,10 +70,10 @@ def add_vertical_row(table: 'PrettyTable', row_name: str, row_value: str):
     if len(row_name) + 2 < max_line_len:
         row_name = row_name.center(max_line_len - 1, ' ')
     exp_line = vertical_char + ' ' + row_name + vertical_char +' ' + row_value
-    e_space = total_line_length - len(exp_line) - 1
+    e_space = total_line_length - len(exp_line)
     opt_line = exp_line + ' ' * e_space + vertical_char
     empty_bottom_space = total_line_length - 2
     fp = len(exp_line) - len(row_value) - 3
-    sp = empty_bottom_space - fp - 1
+    sp = empty_bottom_space - fp
     bottom_line = corner_char +'-'*fp+'+'+'-'*sp+ corner_char
     return opt_line, bottom_line